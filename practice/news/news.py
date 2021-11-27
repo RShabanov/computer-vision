360525c2ee03fd40efd63bc83c7f30ab5e4cbdb8
Shabanov Roman <shabanov.roman0@gmail.com>
2021-11-27 10:37:16 +0800

    Update news

--- practice/news/news.py
+++ practice/news/news.py
@@ -1,38 +1,45 @@
 import cv2
 import numpy as np
 
-# print(cv2.getBuildInformation())
-
 news = cv2.imread("./practice/news/news.jpg")
-chebu = cv2.imread("./practice/news/cheburashka.jpg")
 
-rows, cols, _ = chebu.shape
+cam = cv2.VideoCapture(0)
+if not cam.isOpened():
+    raise RuntimeError("Camera is not working")
 
-pts1 = np.float32([
-    [0, 0],
-    [0, rows],
-    [cols, 0],
-    [cols, rows]
-]) # chebu position to cut
 
-pts2 = np.float32([
-    [19, 25],
-    [39, 294],
-    [434, 51],
-    [434, 266]
-]) # TV position to cut
+# chebu = cv2.imread("./practice/news/cheburashka.jpg")
+#chebu = cv2.resize(chebu, (chebu.shape[0]//2, chebu.shape[1]//2))
 
-M = cv2.getPerspectiveTransform(pts1, pts2)
+cv2.namedWindow("Image")
+while True:
+    ret, chebu = cam.read()
+    rows, cols, _ = chebu.shape
+    pts1 = np.float32([[0,0], [0, rows], [cols, 0], [cols, rows]])
+    pts2 = np.float32([[18, 24], [39, 297], [435, 51], [435, 270]])
+    
+    matrix = cv2.getPerspectiveTransform(pts1, pts2)
+    # print(matrix)
 
-aff_img = cv2.warpPerspective(chebu, M, (cols, rows))[:300, :470]
+    aff_img = cv2.warpPerspective(chebu, matrix, (cols, rows))
+    aff_img = aff_img[:-150, :-150]
+    gray = cv2.cvtColor(aff_img, cv2.COLOR_BGR2GRAY)
+    ret, mask = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)
 
-for y in range(aff_img.shape[0]):
-    for x in range(aff_img.shape[1]):
-        if np.all(aff_img[y, x] == 0): continue
-        news[y, x] = aff_img[y, x]
+    roi = news[:aff_img.shape[0], :aff_img.shape[1]]
+    mask_inv = cv2.bitwise_not(mask)
 
-cv2.namedWindow("Image", cv2.WINDOW_KEEPRATIO)
-cv2.imshow("Image", news)
+    bg = cv2.bitwise_and(roi, roi, mask=mask_inv)
+    # fg = cv2.bitwise_and(chebu, chebu, mask=mask)
+    image = cv2.add(aff_img, bg)
+    news[:image.shape[0], :image.shape[1]] = image
 
-cv2.waitKey(0)
+    cv2.imshow("Image", news)
+    key = cv2.waitKey(1)
+    print(key)
+    
+    if key == ord('q'):
+        break
 cv2.destroyAllWindows()
+
+cam.release()
